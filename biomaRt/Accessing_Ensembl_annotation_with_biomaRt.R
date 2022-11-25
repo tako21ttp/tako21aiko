afa8426359ee1208ee43742c4ecab9d13389b596
takokabe <68540186+takokabe21@users.noreply.github.com>
2022-11-25 21:18:34 +0900

    R

--- biomaRt/Accessing_Ensembl_annotation_with_biomaRt.R
+++ biomaRt/Accessing_Ensembl_annotation_with_biomaRt.R
@@ -1,3 +1,4 @@
+# 1. Introduction
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
@@ -5,17 +6,34 @@ BiocManager::install("biomaRt")
 
 browseVignettes("biomaRt")
 
+# 2. Selecting an Ensembl BioMart database and dataset
+
 # Every analysis with biomaRt starts with selecting a BioMart database to use. The commands below will connect us to Ensembl’s most recent version of the Human Genes BioMart.
 library(biomaRt)
 ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
 
+ensembl
+
 # If this your first time using biomaRt , you might wonder how to find the two arguments we supplied to the useEnsembl() command. This is a two step process, but once you know the setting you need you can use the version shown above as a single command. These initial steps are outlined below.
-#Step1
+
+#Step1: Identifying the database you need
 listEnsembl()
 
 ensembl <- useEnsembl(biomart = "genes")
 ensembl
 
+#Step2: Choosing a dataset
+datasets <- listDatasets(ensembl)
+head(datasets)
+
+searchDatasets(mart = ensembl, pattern = "hsapiens")
+
+ensembl <- useDataset(dataset = "hsapiens_gene_ensembl", mart = ensembl)
+
+ensembl 
+
+
+# 3. How to build a biomaRt query
 
 affyids <- c("202763_at","209310_s_at","207500_at")
 getBM(attributes = c('affy_hg_u133_plus_2', 'entrezgene_id'),
